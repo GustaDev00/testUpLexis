67dd2bdd9351778bbc9f3c918810c9b3e365a686
baby4dev <gustavo.4dev@gmail.com>
2021-01-15 10:40:08 -0300

    ajuste do registo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,28 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="backGreen">
+    <div id="app">
+        <main class="py-4">
+        <div class="account-pages w-100 mt-5 mb-5">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -74,4 +96,3 @@
         </div>
     </div>
 </div>
-@endsection
